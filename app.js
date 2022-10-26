e061ec2f8483f5c26952a9a0df14aee6b7689c1f
Ayelet Hillel <ah3708@tc.columbia.edu>
2022-10-26 19:50:01 -0400

    more css

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function searchGiphy() {
     //save userInput as a variable 
-    var userInput = $('#input').val();
+    var userInput = $('#search-btn').val();
 
     //save Giphy API as a variable 
-    var API_KEY = "qSwLz5wu3cAdpdcewt3XdcFvo77asCUu"
+    var API_KEY = ""
     var BASE_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${userInput}&limit=1&offset=0&rating=g&lang=en`
 
     // make our request
